8ea7b3d5e921b9cf6639a004576f57c5bd86c282
Yanli 盐粒 <yanli@dify.ai>
2026-02-09 19:26:28 +0800

    update the config of basic_os

--- core/src/k/config.py
+++ core/src/k/config.py
@@ -7,5 +7,7 @@ class Config(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="K_")
 
     fs_base: Path
+    basic_os_user: str = "k"
     basic_os_addr: str = "k-container"
     basic_os_port: int = 22
+    basic_os_sshkey: Path = Path(".ssh/id_ed25519")
--- core/src/k/runner_helpers/basic_os.py
+++ core/src/k/runner_helpers/basic_os.py
@@ -8,10 +8,39 @@ class BasicOSHelper:
     config: Config
 
     def command_base(self) -> str:
-        return f'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -T  -i "{self.config.fs_base!s}/.ssh/id_ed25519" -p {self.config.basic_os_port} k@{self.config.basic_os_addr} '
+        return f'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR -tt -i "{self.config.fs_base!s}/{self.config.basic_os_sshkey!s}" -p {self.config.basic_os_port} {self.config.basic_os_user}@{self.config.basic_os_addr} '
 
     def command(self, command: str) -> str:
-        return self.command_base() + command
+        return (
+            f"{self.command_base()} '{command.replace("'", "'").replace('\\', '\\\\')}'"
+        )
 
     def bash_command_non_interactive(self, command: str) -> str:
         return f"{self.command_base()} bash -s <<'K_EOF'\n{command}\nK_EOF"
+
+
+async def main():
+    from k.io_helpers.shell import ShellSession
+
+    config = Config(_env_file=".env")  # type: ignore
+    basic_os_helper = BasicOSHelper(config=config)
+    realcommand = basic_os_helper.command("""
+bash
+python3
+    """)
+    async with ShellSession(realcommand, timeout_seconds=1) as shell:
+        code = None
+
+        stdout, stderr, code = await shell.next()
+        while code is None:
+            print(stdout.decode())
+            print(stderr.decode())
+            stdout, stderr, code = await shell.next((input("> ") + "\n").encode())
+        print(stdout.decode())
+        print(stderr.decode())
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
