ae45d89a1209b32fb153abc7d98dfd2415986b24
Yanli 盐粒 <yanli@dify.ai>
2026-02-10 10:33:08 +0800

    update basic os helper

--- core/src/k/runner_helpers/basic_os.py
+++ core/src/k/runner_helpers/basic_os.py
@@ -12,35 +12,37 @@ class BasicOSHelper:
 
     def command(self, command: str) -> str:
         return (
-            f"{self.command_base()} '{command.replace("'", "'").replace('\\', '\\\\')}'"
+            self.command_base() + "'export TERM=dumb; stty -echo; " + command.replace("'", "'\\''") + "'"
         )
 
-    def bash_command_non_interactive(self, command: str) -> str:
-        return f"{self.command_base()} bash -s <<'K_EOF'\n{command}\nK_EOF"
-
-
 async def main():
-    from k.io_helpers.shell import ShellSession
+    from k.io_helpers.shell import ShellSession, ShellSessionOptions
 
-    config = Config(_env_file=".env")  # type: ignore
+    config = Config()  # type: ignore
     basic_os_helper = BasicOSHelper(config=config)
     realcommand = basic_os_helper.command("""
 bash
 python3
     """)
-    async with ShellSession(realcommand, timeout_seconds=1) as shell:
+    async with ShellSession(
+        realcommand, options=ShellSessionOptions(timeout_seconds=1)
+    ) as shell:
         code = None
 
         stdout, stderr, code = await shell.next()
         while code is None:
-            print(stdout.decode())
-            print(stderr.decode())
-            stdout, stderr, code = await shell.next((input("> ") + "\n").encode())
+            print(stdout.decode(), end="")
+            print(stderr.decode(), end="")
+            stdout, stderr, code = await shell.next((input("") + "\n").encode())
         print(stdout.decode())
         print(stderr.decode())
 
 
 if __name__ == "__main__":
     import asyncio
+
+    from dotenv import load_dotenv
+    load_dotenv()
+    load_dotenv("core/.env")
 
     asyncio.run(main())
